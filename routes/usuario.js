cbd81d9f8dd363d439fa8f844e86a1b667c80841
Nicth Giraldo <nicth.giraldo@gmail.com>
2018-05-27 12:37:02 -0500

    se importo el boy-parse y se asigno la creacion de usuarios

--- routes/usuario.js
+++ routes/usuario.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 var Usuario = require('../models/usuario')
 
+// ==========================================
+// obtener todos los usuarios
+// ==========================================
 app.get('/', (req, res, next) => {
 
     Usuario.find({}, 'nombre email img role').exec(
@@ -20,6 +23,39 @@ app.get('/', (req, res, next) => {
             });
 
         });
+});
+
+// ==========================================
+// crear un nuevo usuarios 
+// ==========================================
+app.post('/', (req, res) => {
+
+    var body = req.body;
+
+    var usuario = new Usuario({
+        nombre: body.nombre,
+        email: body.email,
+        password: body.password,
+        img: body.img,
+        role: body.role
+    });
+
+    usuario.save((err, usuarioGuardado) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al crear usuario',
+                errors: err
+            });
+        }
+
+        res.status(201).json({
+            ok: true,
+            usuario: usuario
+        });
+
+    });
+
 });
 
 module.exports = app;

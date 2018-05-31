630fb609b3b3d03e8120a065cf6d8210642e007d
Nicth Giraldo <nicth.giraldo@gmail.com>
2018-05-31 15:16:57 -0500

    se crea el modelo y el crud de hospitales, ademas de aplicar las rutas

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ var appRoutes = require('./routes/app')
 var usuarioRoutes = require('./routes/usuario')
 var loginRoutes = require('./routes/login')
 
+var hospitalRoutes = require('./routes/hospital')
+
 // conexion a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
     if (err) throw err; // si existe algun erro para todos los procesos
@@ -24,6 +26,7 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
 
 // rutas
 app.use('/usuario', usuarioRoutes);
+app.use('/hospital', hospitalRoutes);
 app.use('/login', loginRoutes);
 app.use('/', appRoutes);
 
---
+++ routes/hospital.js
@@ -0,0 +1,142 @@
+var express = require('express');
+var app = express();
+var Hospital = require('../models/hospital')
+
+var mdAutenticacion = require('../middlewares/autenticacion');
+
+// ==========================================
+// obtener todos los hospitales
+// ==========================================
+app.get('/', (req, res, next) => {
+
+    Hospital.find({}).exec(
+        (err, hospitales) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    mensaje: 'Error cargando hospitales',
+                    errors: err
+                });
+            }
+
+            res.status(200).json({
+                ok: true,
+                hospitales: hospitales
+            });
+
+        });
+});
+
+// ==========================================
+// actualizar un hospital
+// ==========================================
+app.put('/:id', mdAutenticacion.verificaToken, (req, res) => {
+
+    var id = req.params.id
+    var body = req.body;
+
+    Hospital.findById(id, (err, hospital) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar hospital',
+                errors: err
+            });
+        }
+
+        if (!hospital) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'el hospital con el id ' + id + ' no existe',
+                errors: err
+            });
+        }
+
+        hospital.nombre = body.nombre;
+        hospital.usuario = req.usuario._id;
+
+        hospital.save((err, hospitalGuardado) => {
+
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'Error al actualizar hospital',
+                    errors: err
+                });
+            }
+
+            res.status(200).json({
+                ok: true,
+                hospital: hospitalGuardado
+            });
+
+        });
+
+    });
+});
+
+// ==========================================
+// crear un nuevo hospital 
+// ==========================================
+app.post('/', mdAutenticacion.verificaToken, (req, res) => {
+
+    var body = req.body;
+
+    var hospital = new Hospital({
+        nombre: body.nombre,
+        usuario: req.usuario._id
+    });
+
+    hospital.save((err, hospitalGuardado) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'Error al crear hospital',
+                errors: err
+            });
+        }
+
+        res.status(201).json({
+            ok: true,
+            hospital: hospitalGuardado
+        });
+
+    });
+
+});
+
+// ==========================================
+// borrar un hospital
+// ==========================================
+app.delete('/:id', mdAutenticacion.verificaToken, (req, res) => {
+
+    var id = req.params.id;
+
+    Hospital.findByIdAndRemove(id, (err, hospitalBorrado) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al borrar hospital',
+                errors: err
+            });
+        }
+
+        if (!hospitalBorrado) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'no existe un hospital con ese id',
+                errors: err
+            });
+        }
+
+        res.status(200).json({
+            ok: true,
+            message: 'el siguiente hospital fue borrado',
+            hospital: hospitalBorrado
+        });
+
+    });
+
+});
+
+module.exports = app;
